49d913dbb584cb013d284dcd18d674b65149ae10
eccz <99056341+eccz@users.noreply.github.com>
2022-02-07 03:19:47 +0300

    Решение нескольких задач недели 5, описания задач в коммннтариях

--- Неделя 5 Кортежи, циклы for и списки/Ряд - 1.py
+++ Неделя 5 Кортежи, циклы for и списки/Ряд - 1.py
@@ -1,3 +1,5 @@
-myTuple = 1, 2, 3, 4
-a, b, c, d = myTuple
-print(a)
+a, b = int(input()), int(input())
+for i in range(a, b + 1):
+    print(i)
+
+# Даны два целых числа A и B (при этом A≤B). Выведите все числа от A до B включительно.
